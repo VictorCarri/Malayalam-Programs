4c555efccd390dd81d3a2693103c6e15aa9e7bd4
Victor Carri <victorcarri@gmail.com>
2020-05-06 00:42:44 -0400

    Converted all of the functors I had written to range-based loops.

--- server/cpp/IoContextPool.cpp
+++ server/cpp/IoContextPool.cpp
@@ -4,7 +4,6 @@
 /* STL */
 #include <stdexcept> // std::runtime_error
 #include <vector> // std::vector
-#include <algorithm> // std::for_each
 #ifdef DEBUG
 #include <iostream> // std::clog
 #endif
@@ -15,9 +14,8 @@
 /* Our headers */
 #include "SharedPtr.hpp" // SHARED_PTR macro
 #include "ThreadClass.hpp" // THREAD_CLASS macro
-#include "functors/ThreadCreator.hpp" // functors::ThreadCreator, to create the Threads we need
+#include "BindFunc.hpp" // BIND_FUNCTION macro
 #include "functors/ThreadJoiner.hpp" // functors::ThreadJoiner, to wait for threads to join
-#include "functors/IOCStopper.hpp" // functors::IOCStopper, to stop all threads
 #include "IoContextPool.hpp" // Class def
 
 /**
@@ -54,59 +52,33 @@ IoContextPool::IoContextPool(std::size_t poolSize) : nextIoCon(0)
 void IoContextPool::run()
 {
 	std::vector<SHARED_PTR<THREAD_CLASS>> threads;
-	/*#ifdef DEBUG
-	short threadNum = 0;
-	#endif
-
-	*//* Create a pool of threads to run each io_context */
-	/*#ifdef DEBUG
-	std::for_each(ioContexts.cbegin(), ioContexts.cend(), [&threads, &threadNum](iocPtr ptr)
-	#else
-	std::for_each(ioContexts.cbegin(), ioContexts.cend(), [&threads](iocPtr ptr)
-	#endif
-		{
-			boost::shared_ptr<boost::thread> thread(
-			//std::shared_ptr<std::thread> thread(
-				new boost::thread(
-				//new std::thread(
-					boost::bind(
-					//std::bind(
-						&boost::asio::io_context::run,
-						ptr
-					)
-				)
-			);
-			#ifdef DEBUG
-			std::clog << "IoContextPool::run: started thread #" << (threadNum+1) << std::endl;
-			++threadNum;
-			#endif
-			threads.push_back(thread);
-		}
-	);
-
 	#ifdef DEBUG
-	threadNum = 0; // Reset index for next loop
-	#endif*/
-	std::for_each(ioContexts.cbegin(), ioContexts.cend(), functors::ThreadCreator(&threads)); // Create the threads
+	int tno = 1; // Thread #
+	#endif
+
+	/* Create the threads */
+	for (auto ptr : ioContexts)
+	{
+		SHARED_PTR<THREAD_CLASS> thread(
+			new THREAD_CLASS(
+				BIND_FUNCTION(
+					&boost::asio::io_context::run,
+					ptr
+				)
+			)
+		);
+		#ifdef DEBUG
+		std::cout << "IoContextPool::run: created thread #" << tno << std::endl;
+		++tno;
+		#endif
+		threads.push_back(thread);
+	}
 
 	/* Wait for all threads to exit */
-	/*#ifdef DEBUG
-	std::for_each(threads.cbegin(), threads.cend(), [&threadNum](boost::shared_ptr<boost::thread> ptr)
-	//std::for_each(threads.cbegin(), threads.cend(), [&threadNum](std::shared_ptr<std::thread> ptr)
-	#else
-	std::for_each(threads.cbegin(), threads.cend(), [](boost::shared_ptr<boost::thread> ptr)
-	//std::for_each(threads.cbegin(), threads.cend(), [](std::shared_ptr<std::thread> ptr)
-	//std::for_each(threads.cbegin(), threads.cend(), [](std::shared_ptr<boost::thread> ptr)
-	#endif
-		{
-			ptr->join();
-			#ifdef DEBUG
-			std::clog << "IoContextPool::run: thread #" << (threadNum+1) << " has joined." << std::endl;
-			++threadNum;
-			#endif
-		}
-	);*/
-	std::for_each(threads.cbegin(), threads.cend(), functors::ThreadJoiner());
+	for (auto ptr : threads)
+	{
+		ptr->join();
+	}
 }
 
 /**
@@ -114,25 +86,10 @@ void IoContextPool::run()
 **/
 void IoContextPool::stop()
 {
-	#ifdef DEBUG
-	short threadNum = 0;
-	#endif
-
-	// Explicitly stop all io_contexts
-	/*#ifdef DEBUG
-	std::for_each(ioContexts.cbegin(), ioContexts.cend(), [&threadNum](iocPtr ptr)
-	#else
-	std::for_each(ioContexts.cbegin(), ioContexts.cend(), [](iocPtr ptr)
-	#endif
-		{
-			ptr->stop();
-			#ifdef DEBUG
-			std::clog << "IoContextPool::stop: stopped thread #" << (threadNum+1) << std::endl;
-			++threadNum;
-			#endif
-		}
-	);*/
-	std::for_each(ioContexts.cbegin(), ioContexts.cend(), functors::IOCStopper());
+	for (auto ptr : ioContexts)
+	{
+		ptr->stop();
+	}
 }
 
 /**
